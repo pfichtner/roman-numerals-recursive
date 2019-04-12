0b954915b0e2b6165a4a9a76b0849e1444926533
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2019-04-12 19:42:10 +0200

    initial revision

---
+++ src/romnumkata/ArabToRomTest.java
@@ -0,0 +1,16 @@
+package romnumkata;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class ArabToRomTest {
+
+	@Test
+	void test() {
+		fail();
+	}
+
+}
